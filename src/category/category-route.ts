29531c4e21c2e0818d2a4ae1067f83bb49408793
Maulik <mb.dev08@gmail.com>
2025-07-28 10:28:01 +0530

    feat:authorization added

--- src/category/category-route.ts
+++ src/category/category-route.ts
@@ -8,35 +8,42 @@ import { CategoryService } from "./category-service";
 import { CategoryController } from "./category-controller";
 import categoryValidator from "./category-validator";
 import authentication from "../common/middlewares/authentication";
+import { routeProtection } from "../common/middlewares/routeProtection";
+import { ROLES } from "../constants";
 
 const CategoryRouter = express.Router();
 
 const categoryService = new CategoryService();
 const categoryController = new CategoryController(categoryService);
 
-CategoryRouter.post("/", authentication, categoryValidator, (async (
-    req: Request,
-    res: Response,
-    next: NextFunction,
-) => {
-    await categoryController.create(req, res, next);
-}) as RequestHandler);
+CategoryRouter.post(
+    "/",
+    authentication,
+    routeProtection([ROLES.ADMIN, ROLES.MANAGER]),
+    categoryValidator,
+    (async (req: Request, res: Response, next: NextFunction) => {
+        await categoryController.create(req, res, next);
+    }) as RequestHandler,
+);
 
-CategoryRouter.put("/:id", authentication, categoryValidator, (async (
-    req: Request,
-    res: Response,
-    next: NextFunction,
-) => {
-    await categoryController.update(req, res, next);
-}) as RequestHandler);
+CategoryRouter.put(
+    "/:id",
+    authentication,
+    routeProtection([ROLES.ADMIN, ROLES.MANAGER]),
+    categoryValidator,
+    (async (req: Request, res: Response, next: NextFunction) => {
+        await categoryController.update(req, res, next);
+    }) as RequestHandler,
+);
 
-CategoryRouter.delete("/:id", authentication, (async (
-    req: Request,
-    res: Response,
-    next: NextFunction,
-) => {
-    await categoryController.delete(req, res, next);
-}) as RequestHandler);
+CategoryRouter.delete(
+    "/:id",
+    authentication,
+    routeProtection([ROLES.ADMIN, ROLES.MANAGER]),
+    (async (req: Request, res: Response, next: NextFunction) => {
+        await categoryController.delete(req, res, next);
+    }) as RequestHandler,
+);
 
 CategoryRouter.get("/", authentication, (async (
     req: Request,
